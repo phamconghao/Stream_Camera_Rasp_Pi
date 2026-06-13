780d022385a64c400b9e34b46b7da98b2c7a4dd4
HaoPC <conghao.cha2009@gmail.com>
2026-06-13 22:27:39 +0700

    Configure Camera

--- src/capture/camera_capture.cpp
+++ src/capture/camera_capture.cpp
@@ -2,6 +2,7 @@
 #include <memory>
 #include <thread>
 #include <chrono>
+#include <vector>
 
 #include <libcamera/libcamera.h>
 
@@ -10,7 +11,49 @@
 using namespace libcamera;
 static std::unique_ptr<CameraManager> g_camera_manager;
 static std::shared_ptr<Camera> g_camera;
+static std::unique_ptr<CameraConfiguration> g_config;
+static std::unique_ptr<FrameBufferAllocator> g_allocator;
+static Stream *g_stream;
+static std::vector<std::unique_ptr<Request>> g_requests;
 static bool g_running = false;
+static uint64_t g_frame_count = 0;
+// static std::mutex g_camera_mutex;
+
+static void request_complete(Request *request)
+{
+    if (!g_running)
+    {
+        return;
+    }
+
+    std::cout << "[CALLBACK] request complete" << std::endl;
+
+    if (request->status() == Request::RequestCancelled)
+    {
+        return;
+    }
+
+    g_frame_count++;
+
+    std::cout << "[CAPTURE] Frame #" << g_frame_count << std::endl;
+
+    const Request::BufferMap &buffers = request->buffers();
+
+    for (auto const &pair : buffers)
+    {
+        FrameBuffer *buffer = pair.second;
+        const FrameMetadata &meta = buffer->metadata();
+        std::cout << "[CAPTURE] byteused = " << meta.planes()[0].bytesused << std::endl;
+    }
+
+    // Requeue requests
+    request->reuse(Request::ReuseBuffers);
+
+    if (g_running)
+    {
+        g_camera->queueRequest(request);
+    }
+}
 
 int camera_capture_init(void)
 {
@@ -49,6 +92,69 @@ int camera_capture_init(void)
 
     std::cout << "[CAPTURE] acquire OK" << std::endl;
 
+    // Generate config
+    g_config = g_camera->generateConfiguration({ StreamRole::Viewfinder });
+
+    if (!g_config)
+    {
+        std::cerr << "[CAPTURE] generateConfiguration failed" << std::endl;
+
+        return -1;
+    }
+
+    // Set resolution
+    StreamConfiguration &cfg = g_config->at(0);
+    cfg.size.width = 640;
+    cfg.size.height = 480;
+    cfg.pixelFormat = formats::YUV420;
+
+    // Validate
+    CameraConfiguration::Status status = g_config->validate();
+    if (status == CameraConfiguration::Invalid)
+    {
+        std::cerr << "[CAPTURE] Invalid configuration" << std::endl;
+        return -1;
+    }
+
+    // Configure camera
+    if (g_camera->configure(g_config.get()) < 0)
+    {
+        std::cerr << "[CAPTURE] configure failed" << std::endl;
+        return -1;
+    }
+
+    // Save stream pointer
+    g_stream = cfg.stream();
+
+    // Allocator buffers
+    g_allocator = std::make_unique<FrameBufferAllocator>(g_camera);
+    if (g_allocator->allocate(g_stream) < 0)
+    {
+        std::cerr << "[CAPTURE] allocate failed" << std::endl;
+        return -1;
+    }
+
+    // Connect callback
+    g_camera->requestCompleted.connect(request_complete);
+
+    // Create requests
+    const auto &buffers = g_allocator->buffers(g_stream);
+    for (auto &buffer : buffers)
+    {
+        std::unique_ptr<Request> request = g_camera->createRequest();
+        if (!request)
+        {
+            continue;
+        }
+
+        if (request->addBuffer(g_stream, buffer.get()) < 0)
+        {
+            continue;
+        }
+
+        g_requests.push_back(std::move(request));
+    }
+
     return 0;
 }
 
@@ -56,6 +162,19 @@ int camera_capture_start(void)
 {
     std::cout << "[CAPTURE] start" << std::endl;
     g_running = true;
+    
+    if (g_camera->start() < 0)
+    {
+        std::cerr << "[CAPTURE] start failed" << std::endl;
+        return -1;
+    }
+
+    for (auto &request : g_requests)
+    {
+        g_camera->queueRequest(request.get());
+    }
+
+    std::cout << "[CAPTURE] Streaming..." << std::endl;
 
     return 0;
 }
@@ -63,12 +182,35 @@ int camera_capture_start(void)
 void camera_capture_stop(void)
 {
     std::cout << "[CAPTURE] stop" << std::endl;
+    std::cout << "[STOP] begin" << std::endl;
     g_running = false;
+    g_camera->stop();
+    std::cout << "[STOP] end" << std::endl;
 }
 
 void camera_capture_cleanup(void)
 {
     std::cout << "[CAPTURE] cleanup" << std::endl;
+    std::cout << "[CLEANUP] begin" << std::endl;
+    g_camera->requestCompleted.disconnect();
+
+    if (g_requests.size() > 0)
+    {
+        // Clear requests
+        g_requests.clear();
+    }
+
+    if (g_allocator)
+    {
+        // Release camera allocator
+        g_allocator.reset();
+    }
+
+    if (g_config)
+    {
+        // Release camera config
+        g_config.reset();
+    }
 
     if (g_camera)
     {
@@ -83,4 +225,6 @@ void camera_capture_cleanup(void)
         g_camera_manager->stop();
         g_camera_manager.reset();
     }
+
+    std::cout << "[CLEANUP] end" << std::endl;
 }
